5fdd9eea7145a200991a9171f0e924e6759be6ac
blvckmetxl <82678224+blvckmetxl@users.noreply.github.com>
2021-07-09 12:08:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,13 +41,9 @@ then
 	pacman -S zsh
 fi
 
-sleep 1
-
 [ ! -d "/home/bm/.oh-my-zsh" ] && curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O && sed -i 's/RUNZSH=${RUNZSH:-yes}/RUNZSH=${RUNZSH:-no}/g' install.sh && chmod +x install.sh && chown bm:bm install.sh && sudo -u bm ./install.sh && rm install.sh
 mv bm.zsh-theme /home/bm/.oh-my-zsh/custom/themes
 mv .zshrc /home/bm
-
-sleep 1
 
 mv wallpapers /home/bm
 mv scripts /home/bm
@@ -55,9 +51,13 @@ chmod +x /home/bm/scripts/chwp.sh /home/bm/scripts/tg.sh
 echo 1 > /home/bm/scripts/gc
 chown bm:bm /home/bm/scripts/gc
 mkdir /home/bm/screenshots
+mkdir /home/bm/vpns
+mkdir /opt/wordlists
+mkdir /etc/feroxbuster
+echo -e "wordlist = \"/opt/wordlists/directory-list-2.3-medium.txt\"\nthreads = 10\nsave_state = false" > /etc/feroxbuster/ferox-config.toml
+mkdir /etc/samba
+touch /etc/samba/smb.conf
 chown bm:bm /home/bm/screenshots
-
-sleep 1
 
 pacman -Qs picom >& /dev/null
 if [ "$?" -ne 0 ]
@@ -66,8 +66,6 @@ then
 	chown root:root picom.conf
 	mv picom.conf /etc/xdg/picom.conf
 fi
-
-sleep 1
 
 echo "_JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'" >> /etc/environment # fix burpsuite weird font
 
@@ -81,38 +79,29 @@ else
 	mv .config/* /home/bm/.config
 fi
 
-sleep 1
-
 pacman -S --noconfirm firefox ripgrep alacritty xterm unzip tcpdump wget dialog i3-gaps rofi xcursor-simpleandsoft hicolor-icon-theme lxappearance-gtk3 xorg-server thunar discord openvpn feh scrot gparted reflector tk lightdm lightdm-gtk-greeter python2 cronie pkgfile libpulse noto-fonts noto-fonts-cjk noto-fonts-emoji pulseaudio libpulse python-pip xdg-utils gvfs gvfs-afc # needed for thunar to show my usb
 sed -i 's/autospawn = no/autospawn = yes/g' /etc/pulse/client.conf # fix pulseaudio config
 sed -i 's/\/usr\/bin\/gparted/\/usr\/bin\/sudo \/usr\/bin\/gparted/g' /usr/share/applications/gparted.desktop # workaround not running polkit
 chown root:root solarized-darker.rasi
 mv solarized-darker.rasi /usr/share/rofi/themes
 echo "* * * * * root /usr/bin/pkgfile --update" >> /etc/cron.d/0hourly
-mkdir -p /home/bm/.icons/default
+mkdir -p /home/bm/.icons/default && chown -R bm:bm /home/bm/.icons
 echo -e "[Icon Theme]\nName=Default\nComment=Default Cursor Theme\nInherits=Simple-and-Soft" > /home/bm/.icons/default/index.theme
-ln -s /usr/share/icons/Simple-and-Soft/cursors ~/.icons/default/cursors
+chown bm:bm /home/bm/.icons/default/index.theme
+ln -s /usr/share/icons/Simple-and-Soft/cursors /home/bm/.icons/default/cursors
 curl -O https://raw.githubusercontent.com/jluttine/rofi-power-menu/master/rofi-power-menu && chmod +x rofi-power-menu
 sed -i 's/all=(shutdown reboot suspend hibernate logout lockscreen)/all=(shutdown reboot logout suspend)/g' rofi-power-menu && mv rofi-power-menu /usr/bin
 systemctl enable lightdm
-systemclt enable --now cronie
-
-sleep 1
+systemctl enable --now cronie
 
 sudo -u bm yay -S bumblebee-status neovim nerd-fonts-mononoki suru-plus-git
 
-sleep 1
-
-sudo -u bm yay --nopgpfetch --mflags --skipinteg spotify spotify-adblock
+sudo -u bm yay --nopgpfetch --mflags --skipinteg spotify spotify-adblock tor-browser
 chown root:root spotify.desktop
 mv spotify.desktop /usr/share/applications
 rm /usr/share/applications/spotify-adblock.desktop
 
-sleep 1
-
 pip install dbus-python # needed for bumblebee-status spotify module
-
-sleep 1
 
 curl https://blackarch.org/strap.sh | sh
 sudo kill -9 `pidof pulseaudio` && pulseaudio
