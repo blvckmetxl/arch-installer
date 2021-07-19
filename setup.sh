58813a4913190cb49f6feb8d558314c0cb6c7c79
blvckmetxl <82678224+blvckmetxl@users.noreply.github.com>
2021-07-19 02:46:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,6 +58,7 @@ echo -e "wordlist = \"/opt/wordlists/directory-list-2.3-medium.txt\"\nthreads = 
 mkdir /etc/samba
 touch /etc/samba/smb.conf
 chown bm:bm /home/bm/screenshots
+mv wallpapers /home/bm
 
 pacman -Qs picom >& /dev/null
 if [ "$?" -ne 0 ]
@@ -74,7 +75,7 @@ mv .config /home/bm
 
 pacman -S --noconfirm firefox ripgrep netcat alacritty xterm unzip tcpdump wget dialog sxhkd bspwm qbittorrent awesome-terminal-fonts arandr 
 vlc rofi xcursor-simpleandsoft lxappearance-gtk3 xorg-server thunar discord openvpn feh flameshoburpburpt gparted reflector lightdm rlwrap
-lightdm-gtk-greeter cronie pkgfile noto-fonts noto-fonts-cjk noto-fonts-emoji pulseaudio python-pip xdg-utils gvfs gvfs-afc xsel
+lightdm-gtk-greeter cronie pkgfile noto-fonts noto-fonts-cjk noto-fonts-emoji pulseaudio python-pip xdg-utils gvfs gvfs-afc xsel ranger
 
 sed -i 's/autospawn = no/autospawn = yes/g' /etc/pulse/client.conf # fix pulseaudio config
 sed -i 's/\/usr\/bin\/gparted/\/usr\/bin\/sudo \/usr\/bin\/gparted/g' /usr/share/applications/gparted.desktop
