b7a1b7234e36f50aa9169ba542d825503678d38b
blvckmetxl <82678224+blvckmetxl@users.noreply.github.com>
2021-07-28 21:31:12 +0000

    Create setup.sh

---
+++ archcraft/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mv sxhkdrc /home/bm/.config/sxhkd/
+mv config /home/bm/.config/bspwm/polybar/
+mv .zshrc /home/bm/ && chmod +x /home/bm/.zshrc
+mv .fehbg /home/bm/ && chmod +x /home/bm/.fehbg
+mv bspwmrc /home/bm/.config/bspwm/ && chmod +x /home/bm/.config/bspwm/bspwmrc
