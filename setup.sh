c7a1c697e1a222dafcbabd24910ab19405b3d4e2
blvckmetxl <82678224+blvckmetxl@users.noreply.github.com>
2021-07-28 22:13:12 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,32 +3,6 @@
 if [[ "$(cat /etc/os-release | grep \"Archcraft\")" ]]
 then
     	chmod +x archcraft/setup.sh && cd archcraft && ./setup.sh
-    
-    	mkdir /opt/wordlists
-    	wget https://github.com/praetorian-inc/Hob0Rules/raw/master/wordlists/rockyou.txt.gz
-    	gzip -d rockyou.txt.gz
-    	mv rockyou.txt /opt/wordlists
-	curl -O https://raw.githubusercontent.com/daviddias/node-dirbuster/master/lists/directory-list-2.3-medium.txt
-	mv directory-list-2.3-medium.txt /opt/wordlists
-	mkdir /etc/feroxbuster
-	echo -e "wordlist = \"/opt/wordlists/directory-list-2.3-medium.txt\"\nthreads = 10\nsave_state = false" > /etc/feroxbuster/ferox-config.toml
-    
-    	pacman -S --noconfirm firefox discord ripgrep tcpdump python-pip qbittorrent xcursor-simpleandsoft pkgfile xorg-xsetroot
-	pkgfile --update
-	
-	echo -e "[Icon Theme]\nName=Default\nComment=Default Cursor Theme\nInherits=Simple-and-Soft" > /home/bm/.icons/default/index.theme
-	sed -i 's/Inherits.*/Inherits=Simple-and-Soft/g' /usr/share/icons/default/index.theme
-	echo "gtk-cursor-theme-name=Simple-and-Soft" >> /usr/share/gtk-3.0/settings.ini
-    
-    	echo -e "JAVA_AWT_WM_NONREPARENTING=1" >> /etc/environment
-	
-	### tools ###
-	pacman -S --noconfirm aircrack-ng exploitdb hydra john metasploit wireshark-qt
-	yay -S --noconfirm --mflags burpsuite feroxbuster rustscan wfuzz 
-	###   -   ###
-	
-	sudo -u bm yay -S --noconfirm --removemake neovim spotify spotify-adblock tor-browser teams-for-linux
-	
 	exit
 fi
 
